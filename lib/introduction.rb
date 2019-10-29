4f5c9d91b9d263b49d54f93722bb77f0b0f96f7f
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-10-29 03:55:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,9 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-def introduction_with_language_optional (name, language="Ruby")
-  puts 
+def introduction_with_language_optional (Markita, language="Ruby")
+  puts "Hi my name is #{name} and I am learningto program in #{language}"
+end
 
 
 
